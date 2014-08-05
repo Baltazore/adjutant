1d6d4af9546d50a24479eb4cb7011707ee59e9a1
Baltazore <baltazore92@gmail.com>
2014-08-05 19:29:39 +0300

    Add root module require in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
+require 'adjutant'
+
 RSpec.configure do |config|
   # Many RSpec users commonly either run the entire suite or an individual
   # file, and it's useful to allow more verbose output when running an
